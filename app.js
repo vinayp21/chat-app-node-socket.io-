ea8f18e62a3c2cfaa3b46626a9d5b58edbe9449a
vinayp321 <vinprakash@deloitte.com>
2018-03-22 20:54:17 +0530

    chat app

--- app.js
+++ app.js
@@ -17,21 +17,21 @@ app.use('/', function(req, res, next){
 	res.sendFile(INDEX);
 })
 
-// io.on('connection', function(client){
-// 	client.on('join', function(name){
-// 		client.nickname=name;
-// 	});
-// 	client.on('messages',function(data){
-// 	var obj={
-// 		msg:data,
-// 		sender:client.nickname
-// 	}
-// 	client.broadcast.emit('messages',obj);
-// 	var senderobj={
-// 		msg:data,
-// 		sender:'You'
-// 	}
-// 	client.emit('messages',senderobj);
-// 	});
-//
-// });
+io.on('connection', function(client){
+	client.on('join', function(name){
+		client.nickname=name;
+	});
+	client.on('messages',function(data){
+	var obj={
+		msg:data,
+		sender:client.nickname
+	}
+	client.broadcast.emit('messages',obj);
+	var senderobj={
+		msg:data,
+		sender:'You'
+	}
+	client.emit('messages',senderobj);
+	});
+
+});
