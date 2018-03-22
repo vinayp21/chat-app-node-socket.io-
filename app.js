4c1e9b3d370ad8f55097a80077b03df4b1545659
vinayp321 <vinprakash@deloitte.com>
2018-03-22 21:05:48 +0530

    test

--- app.js
+++ app.js
@@ -6,32 +6,17 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'views/index.html');
-const app =express();
-const server = require('http').createServer(app);
 
-server.listen(3000);
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
-app.use(express.static(__dirname +'/public'));
-app.use('/', function(req, res, next){
-	res.sendFile(INDEX);
-})
 
-io.on('connection', function(client){
-	client.on('join', function(name){
-		client.nickname=name;
+io.on('connection', (socket) => {
+  console.log('Client connected');
+	socket.emit('news', {data:'hello'});
+  socket.on('client', (res) => {
+		socket.emit('news', {data: res.data });
 	});
-	client.on('messages',function(data){
-	var obj={
-		msg:data,
-		sender:client.nickname
-	}
-	client.broadcast.emit('messages',obj);
-	var senderobj={
-		msg:data,
-		sender:'You'
-	}
-	client.emit('messages',senderobj);
-	});
-
 });
