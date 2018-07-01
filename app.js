1f8a254cfd3a227ac01aaa38c89596431bb8eb79
vinayp321 <vinprakash@deloitte.com>
2018-07-01 12:40:16 +0530

    chat app

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
 
-const INDEX = path.join(__dirname, 'views/index.html');
-const app=express();
-const server= require('http').createServer(app);
+const INDEX = path.join(__dirname, "views/index.html");
+const app = express();
+const server = require("http").createServer(app);
+const PORT = process.env.PORT || 3000;
 
-// const server = express()
-//   .use('/',(req, res) => res.sendFile(INDEX) )
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-const PORT= process.env.PORT || 3000
 const io = socketIO(server);
-server.listen(PORT);
-app.get('/',function(req,res){
-	res.sendFile(INDEX);
-})
-io.on('connection', (socket) => {
-  console.log('Client connected');
-	socket.emit('news', {data:'hello'});
-  socket.on('client', (res) => {
-		socket.emit('news', {data: res.data });
-	});
+server.listen(3000);
+app.use(express.static(__dirname + "/public"));
+app.use("/", function(req, res, next) {
+  res.sendFile(INDEX);
+});
+
+io.on("connection", function(client) {
+  client.on("join", function(name) {
+    client.nickname = name;
+  });
+  client.on("messages", function(data) {
+    var obj = {
+      msg: data,
+      sender: client.nickname
+    };
+    client.broadcast.emit("messages", obj);
+    var senderobj = {
+      msg: data,
+      sender: "You"
+    };
+    client.emit("messages", senderobj);
+  });
 });
